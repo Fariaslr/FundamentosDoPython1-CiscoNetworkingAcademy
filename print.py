238c5a231296cb8c0d53b0e209fb0d6d9149fe14
Fariaslr <lucasfarias485@gmail.com>
2025-08-24 11:06:40 -0300

    :tada: Iniciando repo do curso de Python da Cisco

---
+++ print.py
@@ -0,0 +1,112 @@
+print("Olá mundo!")
+#print(Olá mundo!)
+#print"me poupe"
+#print('Olá mundo!') print('Olá Mel!')
+print ("lucas farinha")
+
+print("A pequenina aranha escalou a tromba d'água.")
+print()
+print("Caiu a chuva e lavou a aranha.")
+
+
+print('a dona aranha subiu \npela parede')
+print('veio a chuva forte \ne a derrubou')
+
+print("loja maresia" , "croche.")
+
+print("Meu nome é", "Python.")
+print("Monty Python.")
+
+print("Meu nome é", "Python.", end=" ")
+print("Monty Python.")
+
+print("Meu nome é ", end="")
+print("Monty Python.")
+
+print("Meu", "nome", "é", "Monty", "Python.", sep="-")
+
+print("Meu", "nome", "é", "Monty", "Python.", sep=" lucas ")
+
+print("Meu", "nome", "é", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+print("Programação","Essenciais","em",sep='***', end='...')
+print("Python")
+
+print("\n **")
+print(" **")
+print(" **")
+print(" **")
+print(" **")
+print(" ** ")
+print(" *********")
+print(" *********")
+
+
+# Solução de amostra
+
+###################
+print("versão original:")
+###################
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+###################
+print("com menos 'print()' invocações:")
+###################
+print("    *\n   * *\n  *   *\n *     *\n***   ***")
+print("  *   *\n  *   *\n  *****")
+###################
+print("mais alto:")
+###################
+print("        *")
+print("       * *")
+print("      *   *")
+print("     *     *")
+print("    *       *")
+print("   *         *")
+print("  *           *")
+print(" *             *")
+print("******     ******")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *     *")
+print("     *******")
+###################
+print("dobrou:")
+###################
+print("        *         "*2)
+print("       * *        "*2)
+print("      *   *       "*2)
+print("     *     *      "*2)
+print("    *       *     "*2)
+print("   *         *    "*2)
+print("  *           *   "*2)
+print(" *             *  "*2)
+print("******     ****** "*2)
+print("     *     *      "*2)
+print("     *     *      "*2)
+print("     *     *      "*2)
+print("     *     *      "*2)
+print("     *     *      "*2)
+print("     *     *      "*2)
+print("     *******      "*2)
+
+print("Meu\nnome\né\nBond.", end=" ")
+print("James Bond.")
+ 
+#print(sep="&", "peixe", "salgadinhos")
+ 
+print('Greg\'s book.')
+print("'Greg's book.'")
+print('Greg\'s book.')
+print("Greg\'s book.")
+print('Greg')
